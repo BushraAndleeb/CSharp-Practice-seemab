d7c68c2bce47cb3233c48899a2a36a5115579c2f
Seemab Altaf <109847357+SeemabAltaf@users.noreply.github.com>
2022-07-23 13:13:06 +0500

    2nd program in C#.

---
+++ do while loop.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace batch001_do.@while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int i = 1;
+            
+            do
+            {
+                Console.WriteLine (i);
+                i++;
+            }
+            while (i<0);
+            Console.WriteLine ("for loop terminates...");
+            Console.ReadLine();
+        }
+    }
+}
