9e0080229406e02bea2d6777aa620fb5a6ea8dbb
Seemab Altaf <109847357+SeemabAltaf@users.noreply.github.com>
2022-07-23 13:04:00 +0500

    First Program in repo.

---
+++ While.cs
@@ -0,0 +1,17 @@
+using System;
+namespace CSharpBatch001
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int i = 1;
+            while (i<=5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
